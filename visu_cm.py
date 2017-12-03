fcc241c8e338e1e7b413cfd2e3c69602016af949
Pharrell.zx <15113029g@connect.polyu.hk>
2017-12-03 16:17:13 +0800

    Format .svg is not supported by latex graphics. Export .eps instead.

--- visu_cm.py
+++ visu_cm.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import os
 
 
-def visu_confusion_matrix(file_name, folder, target_format):
+def visu_confusion_matrix(file_name, folder, target_format, num_classes):
 
     df = pd.read_csv(file_name, sep=',', header=None)
-    df_cm = pd.DataFrame(df.values, index=list(range(0, 37)),
-                         columns=list(range(0, 37)))
+    df_cm = pd.DataFrame(df.values, index=list(range(0, num_classes)),
+                         columns=list(range(0, num_classes)))
     plt.figure(figsize=(10, 8))
-    sns.heatmap(df_cm, annot=False, cmap=sns.color_palette("Greens"))
+    sns.heatmap(df_cm, annot=False, cmap=sns.color_palette("Blues"))
 
     svg_basename = os.path.basename(file_name)
     svg_basename_new = os.path.splitext(svg_basename)[0] + '.' + target_format
@@ -28,14 +28,18 @@ if __name__ == "__main__":
     # visu_confusion_matrix('csv/ckpt-10342.csv')
     # visu_confusion_matrix('csv/ckpt-10342.csv')
     file_names = [
-        'csv/ckpt-10342.csv',
-        'csv/ckpt-20622.csv',
-        'csv/ckpt-30757.csv',
-        'csv/ckpt-40884.csv',
-        'csv/ckpt-51009.csv',
-        'csv/ckpt-61168.csv',
+        # 'csv/ckpt-10342.csv',
+        # 'csv/ckpt-20622.csv',
+        # 'csv/ckpt-30757.csv',
+        # 'csv/ckpt-40884.csv',
+        # 'csv/ckpt-51009.csv',
+        # 'csv/ckpt-61168.csv',
+        'csv/08-kpt-133049.csv',
+        'csv/16-kpt-304857.csv'
     ]
     for name in file_names:
         visu_confusion_matrix(file_name=name,
                               folder='figures',
-                              target_format='eps')
+                              target_format='eps',
+                              num_classes=32,
+                              )
